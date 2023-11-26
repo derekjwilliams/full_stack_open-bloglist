145bd9ec958c5a91b95f96b7d12a5bb010a03ce2
derek williams <derek61@gmail.com>
2023-11-26 06:32:25 -0700

    initial commit

---
+++ bloglist-frontend/src/App.jsx
@@ -0,0 +1,92 @@
+import { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import Notification from './components/Notification'
+import blogService from './services/blogs'
+import loginService from './services/login'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [user, setUser] = useState(null)
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  useEffect(() => {
+    blogService.getAll().then((blogs) => {setBlogs(blogs)})
+  }, [])
+
+  const Blogs = () => (
+    <div>
+      <h2>blogs</h2>
+      {blogs.map((blog) => (
+        <Blog key={blog.id} blog={blog} />
+      ))}
+    </div>
+  )
+
+  const loginForm = () => (
+    <div>
+      <Notification message={errorMessage} />
+      <h1>log in to application</h1>
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+          <input
+            type='text'
+            value={username}
+            name='Username'
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+          <input
+            type='password'
+            value={password}
+            name='Password'
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type='submit'>login</button>
+      </form>
+    </div>
+  )
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login({
+        username,
+        password,
+      })
+
+      window.localStorage.setItem('loggedBlogappUser', JSON.stringify(user))
+      blogService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      // console.log('failed to login') //TODO
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+  return (
+    <div>
+      {!user && loginForm()}
+      {user && (
+        <div>
+          <p>
+            {user.name}({user.username}) logged in
+          </p>
+          {Blogs()}
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default App
---
+++ bloglist-frontend/src/services/blogs.js
@@ -0,0 +1,18 @@
+import axios from 'axios'
+const baseUrl = '/api/blogs'
+
+let token = null
+
+const getAll = () => {
+  const config = {
+    headers: { Authorization: token },
+  }
+  const request = axios.get(baseUrl, config)
+  return request.then(response => response.data)
+}
+
+const setToken = newToken => {
+  token = `Bearer ${newToken}`
+}
+
+export default { getAll, setToken }
