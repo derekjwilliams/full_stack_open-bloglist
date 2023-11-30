7ac6047e55866c7dc1b668d16a3550d17cba14f8
derek williams <derek61@gmail.com>
2023-11-30 16:51:35 -0700

    moved container out of test and into describe block

--- bloglist-frontend/src/components/Blog.test.js
+++ bloglist-frontend/src/components/Blog.test.js
@@ -3,36 +3,41 @@ import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
 import Blog from './Blog'
 
-test('renders content', () => {
-  const blog = {
-    title: 'Test Blog',
-    author: 'Test Author',
-    url: 'https://developer.mozilla.org/en-US/docs/Web/HTML/Element/section',
-    likes: 1,
-    user: {
+describe('Blog component', () => {
+  let container
+
+  beforeEach(() => {
+    const blog = {
+      title: 'Test Blog',
+      author: 'Test Author',
+      url: 'https://developer.mozilla.org/en-US/docs/Web/HTML/Element/section',
+      likes: 1,
+      user: {
+        username: 'John',
+        name: 'John Lennon',
+        id: '6563446d01a8f4f64788555e',
+      },
+      id: '6568889d8c0b6f89f4750588',
+    }
+    const user = {
+      token: 'xxx',
       username: 'John',
       name: 'John Lennon',
-      id: '6563446d01a8f4f64788555e',
-    },
-    id: '6568889d8c0b6f89f4750588',
-  }
-  const user = {
-    token: "xxx",
-    username: "John",
-    name: "John Lennon"
-  }
+    }
+    container = render(<Blog blog={blog} user={user} />).container
+  })
+  test('renders content, showing title and author by default, but not url or likes', () => {
 
-  const { container } = render(<Blog blog={blog} user={user}/>)
+    const titleElement = container.querySelector('.blog-title')
+    expect(titleElement).toBeDefined()
 
-  const titleElement = container.querySelector('.blog-title')
-  expect(titleElement).toBeDefined()
+    const authorElement = container.querySelector('.blog-author')
+    expect(authorElement).toBeDefined()
 
-  const authorElement = container.querySelector('.blog-author')
-  expect(authorElement).toBeDefined()
+    const urlElement = container.querySelector('.blog-url')
+    expect(urlElement).toBeNull()
 
-  const urlElement = container.querySelector('.blog-url')
-  expect(urlElement).toBeNull()
-
-  const likesElement = container.querySelector('.blog-likes')
-  expect(likesElement).toBeNull()
+    const likesElement = container.querySelector('.blog-likes')
+    expect(likesElement).toBeNull()
+  })
 })
