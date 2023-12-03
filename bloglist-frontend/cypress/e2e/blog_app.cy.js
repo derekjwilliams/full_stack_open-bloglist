712545d6e0915e8d9468d938ec3d8b9235448544
derek williams <derek61@gmail.com>
2023-12-03 10:45:23 -0700

    Completed 5.17: bloglist end to end testing, step1

--- bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,5 +1,11 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Bloglist app', function() {
+  beforeEach(function () {
+    cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    cy.visit('http://localhost:5173')
+  })
+
+  it('home page can be opened', function() {
+    cy.visit('http://localhost:5173')
+    cy.contains('Login')
   })
 })
